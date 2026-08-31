f2728f53789456ca49dde2cbf17094092834e5e3
Rahmat Sobrian <135594470+rahmatsobrian@users.noreply.github.com>
2026-08-31 13:49:48 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         // Android 10 (Q) through Android 16
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
